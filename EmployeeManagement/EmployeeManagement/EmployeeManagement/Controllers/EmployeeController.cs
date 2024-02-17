51ca73dd26ac7332a5377e331803e933e019cc68
Coding_Matty <mattia.agus.business@gmail.com>
2024-02-17 22:14:11 +0100

    added a comment

--- EmployeeManagement/EmployeeManagement/EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/EmployeeManagement/EmployeeManagement/Controllers/EmployeeController.cs
@@ -5,6 +5,7 @@ namespace EmployeeManagement.Controllers
 {
     public class EmployeeController : Controller
     {
+        //setting up the dependency injection
         private readonly ApplicationDbContext _dbContext;
         public EmployeeController(ApplicationDbContext dbContext)
         {
