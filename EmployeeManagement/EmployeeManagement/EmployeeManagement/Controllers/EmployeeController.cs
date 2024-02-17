bcebbfa7cae16d07d8a6b539ff9165d500221d84
Mattia Agus <mattia.agus.business@gmail.com>
2024-02-17 22:03:18 +0100

    initial commit

---
+++ EmployeeManagement/EmployeeManagement/EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeManagement.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public EmployeeController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            var employees = _dbContext.Employees.ToList();
+            return View();
+        }
+    }
+}
---
+++ EmployeeManagement/EmployeeManagement/EmployeeManagement/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public DbSet<Employee> Employees { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
